9af6aa0504a7ed105acbad4defcb70a93b5ee347
ratiorine1417 <rataventurine@gmail.com>
2025-08-25 17:29:59 +0900

    1to1

---
+++ src/main/java/com/rookies4/myspringboot/entity/Student.java
@@ -0,0 +1,33 @@
+package com.rookies4.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Student
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    //PK (Sequential Value)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String studentNumber;
+
+    //양방향관계 Student에서 StudentDetail을 참조할 수 있도록 FK에 해당하는 필드명을 mappedBy에 설정한다.
+    //1:1단계 지연로딩
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "student", cascade = CascadeType.ALL)
+    private StudentDetail studentDetail;
+}
